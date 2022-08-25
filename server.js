55e1e968f37dba2e59b1ce69d1ee8cb61aca349d
Dabble362 <75595585+Dabble362@users.noreply.github.com>
2022-08-24 22:09:26 -0400

    refactor server.js

--- server.js
+++ server.js
@@ -1,20 +1,24 @@
 const express = require("express");
 const app = express();
-const connectDB = require("./config/database");
-const homeRoutes = require("./routes/home");
-const characterRoutes = require("./routes/characters");
+// const connectDB = require("./config/database");
+// const homeRoutes = require("./routes/home");
+// const characterRoutes = require("./routes/characters");
 
 require("dotenv").config({ path: "./config/.env" });
 
-connectDB();
+// connectDB();
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use("/", homeRoutes);
-app.use("/characters", characterRoutes);
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+app.get("/characters", (req, res) => {
+  res.sendFile(__dirname + "/views/characters.ejs");
+});
 
 app.listen(process.env.PORT, () => {
   console.log("Server is running!");
